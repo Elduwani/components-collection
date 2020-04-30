82ac7e131a78b51e41502ed6fba532edfc0f52ed
Elduwani <elduwani@gmail.com>
2020-04-30 15:26:53 +0100

    refactoring useRefs

--- src/components/autoInput/AutoInput.jsx
+++ src/components/autoInput/AutoInput.jsx
@@ -14,17 +14,36 @@ export default function AutoInput() {
     const [form, setForm] = useState(formData)
     const [state, setState] = useState(null)
     const [text, setText] = useState("Pay")
-    const refs = {}
+    const xRefs = {}
 
     const collections = ["Card Information", "MM", "DD", "YY", "CVV"]
     useRef(collections.map((_, i) => {
-        const name = collections[i].split(" ")[0].toLowerCase()
-        refs[name] = createRef()
+        // const name = collections[i].split(" ")[0].toLowerCase()
+        xRefs[i] = createRef()
     }))
+
+    let input1 = xRefs[0].current,
+        input2 = xRefs[1].current,
+        input3 = xRefs[2].current,
+        input4 = xRefs[3].current,
+        input5 = xRefs[4].current
 
     function handleChange(e) {
         const { name, value } = e.target
-        setForm({ ...form, [name]: value })
+        const isNumber = Math.sign(value) >= 0
+
+
+        if (isNumber) {
+
+            if (input1.value.length === 3) return input2.focus()
+            if (input2.value.length === 3) return input3.focus()
+            if (input3.value.length === 3) return input4.focus()
+            if (input4.value.length === 3) return input5.focus()
+
+            setForm({ ...form, [name]: value })
+        }
+
+        return
     }
 
     function handleSubmit() {
@@ -32,20 +51,6 @@ export default function AutoInput() {
         setText("Processing")
         console.log(form)
     }
-
-    useEffect(() => {
-        let input1 = refs["card"].current,
-            input2 = refs["mm"].current,
-            input3 = refs["dd"].current,
-            input4 = refs["yy"].current,
-            input5 = refs["cvv"].current
-
-        if (input1.value.length >= 11) input2.focus()
-        if (input2.value.length >= 2) input3.focus()
-        if (input3.value.length >= 2) input4.focus()
-        if (input4.value.length >= 2) input5.focus()
-
-    }, [form]);
 
     useEffect(() => {
         let timer1
@@ -83,7 +88,7 @@ export default function AutoInput() {
                                 placeholder={elem}
                                 onChange={handleChange}
                                 value={form[name]}
-                                ref={refs[name]}
+                                ref={xRefs[i]}
                             />
                         )
                     })
