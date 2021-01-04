d697ad05cf19080d11a1c2c5606218cdd2c6415c
Elduwani <elduwani@gmail.com>
2021-01-04 01:49:01 +0100

    navigation, netflify redirects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom"
+import { Switch, BrowserRouter as Router, Route, Link } from "react-router-dom"
 import Default from "./components/Default"
-import Data from "./components/charts"
 import Calculator from "./components/calculator"
 import Pages from "./components/Pages"
 import Walker from "./components/walker"
@@ -9,11 +8,15 @@ function App() {
   return (
     <Router>
       <div className="body-container">
+        <nav className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/calculator">Calculator</Link>
+          <Link to="/walker">Walker Algo</Link>
+        </nav>
         <Switch>
           <Route exact path='/' component={Pages} />
-          <Route path='/c/calculator' component={Calculator} />
-          <Route path='/c/data' component={Data} />
-          <Route path='/c/walker' component={Walker} />
+          <Route exact path='/calculator' component={Calculator} />
+          <Route exact path='/walker' component={Walker} />
           <Route path='*' component={Default} />
         </Switch>
       </div>
